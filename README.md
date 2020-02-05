cb16048f3fc5ed9dbecbefb35e597568cb1ea777
Baptiste Mazin <baptiste.mazin@gmail.com>
2020-02-05 11:34:47 +0100

    Add test template and README.

---
+++ README.md
@@ -0,0 +1,2 @@
+#Matrix3x3
+Minimal class to do linear algebra with 3x3 matrices.
---
+++ test/Matrix3x3.test.js
@@ -0,0 +1,11 @@
+/* global describe test expect */
+import Matrix from "../src/Matrix3x3.js";
+
+
+describe("Testing Matrix3x3", () => {
+
+    test("Fake test", () => {
+        expect(true).toBe(true);
+    });
+
+});
